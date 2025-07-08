11cc376513da76ba0dae3d15ef38677fe1421c18
RamaTay <ramatayaan@gmail.com>
2025-07-08 15:54:20 +0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       backgroundImage: {
-        'main-bg': "url('https://i.postimg.cc/9Mp7qggs/1-01.png')",
+        'main-bg': "url('https://i.postimg.cc/C5bx9m9m/01-1.png')",
       }
     },
   },
