a4849256da353f137467f83ed8c135fb9d381d90
RamaTay <ramatayaan@gmail.com>
2025-07-08 14:18:24 +0300

    Updated AboutSection.tsx

--- src/components/Home/AboutSection.tsx
+++ src/components/Home/AboutSection.tsx
@@ -38,7 +38,7 @@ const AboutSection = () => {
   }, []);
 
   return (
-    <section id="about" className="py-20 ">
+    <section id="about" className="py-20 bg-main-bg bg-cover bg-center ">
       <div className="container mx-auto px-4">
         <div className="grid grid-cols-1 lg:grid-cols-2 gap-12 items-center">
           {/* Image */}
