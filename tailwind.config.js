ade6f5bd07794f9374f3695ad64702b6ee21d8fc
RamaTay <ramatayaan@gmail.com>
2025-07-08 14:17:34 +0300

    Updated tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        'main-bg': "url('https://i.postimg.cc/N0sTG4LF/01.png')",
+      }
+    },
   },
   plugins: [],
 };
