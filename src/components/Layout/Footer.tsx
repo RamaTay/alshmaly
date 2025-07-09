f5e19fe89a57d63ae52ee4868d9c7ae18185b39d
RamaTay <ramatayaan@gmail.com>
2025-07-09 10:53:15 +0300

    Updated Footer.tsx

--- src/components/Layout/Footer.tsx
+++ src/components/Layout/Footer.tsx
@@ -9,7 +9,7 @@ const Footer = () => {
         <div className="grid grid-cols-1 md:grid-cols-4 gap-8">
           {/* Company Info */}
           <div className="space-y-4">
-            <div className="flex items-center space-x-3"> 
+            <div className="flex items-center space-x-2"> 
                <img 
               src="https://i.postimg.cc/sgdxhP1p/logo.png" 
               alt="Al-Shamaly Logo" 
