5ce99ea1e767f71eea3d441b2ab354645539e0e7
RamaTay <ramatayaan@gmail.com>
2025-07-09 15:46:28 +0300

    Updated ServicesSection.tsx

--- src/components/Home/ServicesSection.tsx
+++ src/components/Home/ServicesSection.tsx
@@ -39,7 +39,7 @@ const ServicesSection = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
           {services.map((service, index) => (
-            <div key={index} className="bg-[#edebe0] border-2 border-transparent rounded-2xl p-8 text-center hover:border-[#b9a779] transition-all duration-500 group shadow-md h-full flex flex-col justify-between">
+            <div key={index} className="bg-[#edebe0] border-4 border-transparent rounded-2xl p-8 text-center hover:border-[#b9a779] transition-all duration-500 group shadow-md h-full flex flex-col justify-between">
               <div>
                 <div className="w-16 h-16 bg-[#054239] text-white rounded-full flex items-center justify-center mx-auto mb-6 group-hover:bg- [#edebe0] group-hover:text-[#edebe0] transition-all duration-300">
                   <service.icon size={32} />
