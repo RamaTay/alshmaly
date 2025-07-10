2f666ba4ea174cf0868afc6551de03335a37349c
RamaTay <ramatayaan@gmail.com>
2025-07-10 11:13:54 +0300

    Updated BlogPostPage.tsx

--- src/pages/BlogPostPage.tsx
+++ src/pages/BlogPostPage.tsx
@@ -35,7 +35,7 @@ const BlogPostPage = () => {
   }
 
   return (
-    <div className="min-h-screen bg-gray-50 pt-20">
+    <div className="min-h-screen bg-[#edebe0] pt-20">
       <div className="container mx-auto px-4 py-8">
         {/* Back Button */}
         <Link 
