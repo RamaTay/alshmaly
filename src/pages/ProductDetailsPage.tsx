57bff0e0980dc543f1c35dbc63a89d689c6a878c
RamaTay <ramatayaan@gmail.com>
2025-07-13 12:12:28 +0300

    Updated ProductDetailsPage.tsx

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -149,9 +149,13 @@ const ProductDetailsPage = () => {
                 {quoteLoading ? 'Submitting...' : 'Request Quote'}
               </button>
 
-              <button className="flex-1 border-2 border-[#b9a779] text-[#b9a779] hover:bg-[#b9a779] hover:text-white py-4 px-6 rounded-full font-semibold transition-all duration-300">
-                Contact Supplier
-              </button>
+             <Link
+  to="/contact"
+  className="w-full block text-center border-2 border-[#b9a779] text-[#b9a779] hover:bg-[#b9a779] hover:text-white py-4 px-6 rounded-full font-semibold transition-all duration-300"
+>
+  Contact Supplier
+</Link>
+
             </div>
 
             {/* Product Features */}
