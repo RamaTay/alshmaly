d5dc6393170bdabc9eb4a46250c96fb80e3a3803
RamaTay <ramatayaan@gmail.com>
2025-07-13 12:26:53 +0300

    Updated ProductDetailsPage.tsx

--- src/pages/ProductDetailsPage.tsx
+++ src/pages/ProductDetailsPage.tsx
@@ -143,7 +143,7 @@ const ProductDetailsPage = () => {
       <div className="mb-8">
   <Link
     to="/contact"
-    className="w-fit  block border-2 border-[#b9a779] text-[#b9a779] hover:bg-[#b9a779] hover:text-white py-3 px-6 rounded-full font-semibold transition-all duration-300"
+    className="flex-1 border-2 border-[#b9a779] text-[#b9a779] hover:bg-[#b9a779] hover:text-white py-4 px-6 rounded-full font-semibold transition-all duration-300"
   >
     Contact Supplier
   </Link>
