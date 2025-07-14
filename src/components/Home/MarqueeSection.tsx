17bfe020cfecbebf6b109da0bee68536a7c9e06d
RamaTay <ramatayaan@gmail.com>
2025-07-14 12:17:27 +0300

    Updated MarqueeSection.tsx

--- src/components/Home/MarqueeSection.tsx
+++ src/components/Home/MarqueeSection.tsx
@@ -9,17 +9,17 @@ const MarqueeSection = () => {
     { icon: Users, text: 'Customer Satisfaction First' },
     { icon: HandHeart, text: 'Ongoing Trader Support' },
   ];
- 
+
   return ( 
     <section className="py-2 bg-[#F7F7F7] border-b-2 border-b-[#edebe0] overflow-hidden w-full">
       <div className="w-full relative">
-        <div className="flex animate-marquee whitespace-nowrap">
+        <div className="flex animate-marquee whitespace-nowrap min-w-max">
           {[...features, ...features, ...features].map((feature, index) => (
             <div 
               key={index} 
-              className="inline-flex items-center mx-8 text-[#054239] flex-shrink-0"
+              className="inline-flex items-center px-12 text-[#054239] flex-shrink-0"
             > 
-              <div className="w-12 h-12 text-[#054239] rounded-full flex items-center justify-center">
+              <div className="w-12 h-12 rounded-full flex items-center justify-center">
                 <feature.icon size={24} />
               </div>
               <span className="ml-4 text-xl font-semibold">{feature.text}</span>
@@ -32,4 +32,3 @@ const MarqueeSection = () => {
 };
 
 export default MarqueeSection;
-
